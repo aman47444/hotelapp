069bc2ad54366463261566c41ae5f9feb1a2a0a5
aman47444 <aman47444>
2019-09-11 17:03:42 +0530

    new file added1111111

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require("mongoose");
-const db = require("./model_database/users.models");
+const db = require("./model/users.model");
 global.bodyParser = require("body-parser");
 const cors = require("cors");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const User = require("./model/users.model");
+const url = "mongodb://localhost/mydb";
 const PORT = 8080;
-app.get('', function (req, res) {
-    res.send("Hello bhai Santosh");
-})
+mongoose.connect(url, { useNewUrlParser: true }, function (err, db) {
+    if (err) throw err;
+    console.log("Databese se connect ho gya");
+    // console.log(db);
+});
+app.post("/login", function (req, res) {
+    console.log(req.body);
+    let email = req.body.username;
+    User.findOne({ username: email }, (err, data) => {
+        if (err) throw err;
+        console.log('LoggedIn')
+        res.send(" data is " + data);
+    });
+});
 app.listen(PORT, function () {
     console.log("Server will listen now " + PORT);
 });
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 class Login extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '', password: ''
+        }
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        let username = e.target.username.value;
+        let password = e.target.password.value;
+        let bodydata = {
+            'username': username,
+            'password': password
+        }
+        console.log(bodydata);
+        axios.post('/login', bodydata)
+            .then(res => console.log(res.status))
+            .then(data => console.log(data));
+
+    }
     render() {
         return (
             <div className="container login_form">
-                <form className="">
+                <form className="" method="POST" onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input type="text" name="username" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username" />
                     </div>
                     <div className="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                     </div>
                     <div className="form-check">
                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                         <label className="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
-                    <button type="submit" className="btn btn-success">Submit</button>
                     <button type="submit" className="btn btn-success">Submit</button>
                 </form>
             </div>
